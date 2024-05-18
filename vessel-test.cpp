9c915c19f45f923f28e43e3e92f96b83ab73c00e
henneboy <henrik_peet@hotmail.com>
2024-05-18 14:14:47 +0200

    vessel ctor test

--- vessel-test.cpp
+++ vessel-test.cpp
@@ -5,13 +5,67 @@
 //#include "number_series.hpp"
 
 #include <doctest/doctest.h>
+#include "Vessel.hpp"
 
 //using namespace data_series;
 
-TEST_CASE("Dummy test case")
+TEST_CASE("Vessel ctor test")
 {
-    CHECK(1==2);
+    auto v = Vessel{"Circadian Rhythm"};
+    CHECK(v.reactions.empty());
+    CHECK(v.vesselName == "Circadian Rhythm");
 }
+/*
+Vessel circadian_rhythm() {
+    const auto alphaA = 50;
+    const auto alpha_A = 500;
+    const auto alphaR = 0.01;
+    const auto alpha_R = 50;
+    const auto betaA = 50;
+    const auto betaR = 5;
+    const auto gammaA = 1;
+    const auto gammaR = 1;
+    const auto gammaC = 2;
+    const auto deltaA = 1;
+    const auto deltaR = 0.2;
+    const auto deltaMA = 10;
+    const auto deltaMR = 0.5;
+    const auto thetaA = 50;
+    const auto thetaR = 100;
+    auto v = Vessel{"Circadian Rhythm"};
+    const auto env = v.environment();
+    const auto DA = v.add("DA", 1);
+    const auto D_A = v.add("D_A", 0);
+    const auto DR = v.add("DR", 1);
+    const auto D_R = v.add("D_R", 0);
+    const auto MA = v.add("MA", 0);
+    const auto MR = v.add("MR", 0);
+    const auto A = v.add("A", 0);
+    const auto R = v.add("R", 0);
+    const auto C = v.add("C", 0);
+    const auto agentMultiOverload = (A + DA);
+    const auto agentAndDelayOverload = (D_A) >> thetaA;
+    const auto reaction = (A + DA) >> gammaA >>= D_A;
+    v.add((A + DA) >> gammaA >>= D_A);
+    v.add(D_A >> thetaA >>= DA + A);
+    v.add((A + DR) >> gammaR >>= D_R);
+    v.add(D_R >> thetaR >>= DR + A);
+    v.add(D_A >> alpha_A >>= MA + D_A);
+    v.add(DA >> alphaA >>= MA + DA);
+    v.add(D_R >> alpha_R >>= MR + D_R);
+    v.add(DR >> alphaR >>= MR + DR);
+    v.add(MA >> betaA >>= MA + A);
+    v.add(MR >> betaR >>= MR + R);
+    v.add((A + R) >> gammaC >>= C);
+    v.add(C >> deltaA >>= R);
+    v.add(A >> deltaA >>= env);
+    v.add(R >> deltaR >>= env);
+    v.add(MA >> deltaMA >>= env);
+    v.add(MR >> deltaMR >>= env);
+    return v;
+}
+*/
+
 /// number_series class
 /*TEST_CASE("Maintain minimum and maximum values")
 {
