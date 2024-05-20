1a06289b21da911f2544331d968261eb5f63e3b9
henneboy <henrik_peet@hotmail.com>
2024-05-20 12:54:54 +0200

    Working symbol table

--- symbol_table.hpp
+++ symbol_table.hpp
@@ -2,6 +2,7 @@
 // Created by Henrik on 18-05-2024.
 //
 #include <unordered_map>
+#include <optional>
 
 #ifndef SHAPE_EXAMPLE_SYMBOL_TABLE_H
 #define SHAPE_EXAMPLE_SYMBOL_TABLE_H
@@ -11,8 +12,17 @@ class [[maybe_unused]] symbol_table{
     std::unordered_map<key, value> table;
 public:
     [[maybe_unused]] bool Add(key k, value v){
-        auto [_, isSuccess] = table.emplace( std::make_pair(k, v));
+        auto [_, isSuccess] = table.insert( std::make_pair(k, v));
         return isSuccess;
+    }
+
+    std::optional<value> Lookup(key k) {
+        auto it = table.find(k);
+        if (it != table.end()) {
+            return it->second; // Found: return the associated value
+        } else {
+            return std::nullopt; // Not found: return an empty optional
+        }
     }
 };
 
--- symbol_table_test.cpp
+++ symbol_table_test.cpp
@@ -8,7 +8,7 @@
 #include <string>
 #include "symbol_table.hpp"
 
-TEST_CASE("Symbol table add tests")
+TEST_CASE("Symbol table add tests (string, unsigned)")
 {
     symbol_table<std::string, unsigned> table{};
     auto key = "key";
@@ -23,4 +23,51 @@ TEST_CASE("Symbol table add tests")
 
     bool success3 = table.Add(key, value);
     CHECK(!success3);
+}
+
+TEST_CASE("Symbol table look up tests (string, unsigned)")
+{
+    symbol_table<std::string, unsigned> table{};
+    auto key = "key";
+    auto otherKey = "otherKey";
+    auto value = 1U;
+
+    bool _ = table.Add(key, value);
+    auto result1 = table.Lookup(key);
+    CHECK(result1.has_value());
+
+    auto result2 = table.Lookup(otherKey);
+    CHECK(!result2.has_value());
+}
+
+TEST_CASE("Symbol table add tests (int, int)")
+{
+    symbol_table<int, int> table{};
+    auto key = 1;
+    auto otherKey = 2;
+    auto value = 42;
+
+    bool success1 = table.Add(key, value);
+    CHECK(success1);
+
+    bool success2 = table.Add(otherKey, value);
+    CHECK(success2);
+
+    bool success3 = table.Add(key, value);
+    CHECK(!success3);
+}
+
+TEST_CASE("Symbol table look up tests (int, int)")
+{
+    symbol_table<int, int> table{};
+    auto key = 1;
+    auto otherKey = 2;
+    auto value = 42;
+
+    bool _ = table.Add(key, value);
+    auto result1 = table.Lookup(key);
+    CHECK(result1.has_value());
+
+    auto result2 = table.Lookup(otherKey);
+    CHECK(!result2.has_value());
 }
