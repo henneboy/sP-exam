6b4212446e0d531215a9540b96b512d542de29bc
henneboy <henrik_peet@hotmail.com>
2024-06-16 00:21:43 +0200

    Lambda to make H visible on graph

--- Executables/seihr_plot_graph.cpp
+++ Executables/seihr_plot_graph.cpp
@@ -11,16 +11,24 @@
 #include "../Observers/PeakObserver.h++"
 #include "../Plotter.hpp"
 
+// Requirement: 5. Demonstrate the application of the library on the three examples (shown in Fig. 3).
+
+
 int main() {
     auto v = seihr();
     std::cout << "Finished building vessel " << v.vesselName << std::endl;
 
     Simulator sim{1};
 
-    //std::vector<std::string> agentsOfInterest = {"S", "E", "I", "H", "R"};
-    std::vector<std::string> agentsOfInterest = {"H"};
+    std::vector<std::string> agentsOfInterest = {"S", "E", "I", "H", "R"};
+    //std::vector<std::string> agentsOfInterest = {"H"};
     StateMemorizer memorizer{agentsOfInterest};
     sim.simulate(100.0, v, memorizer);
+    std::vector<DataPoint> transformedData{};
+    std::ranges::for_each(memorizer.data.begin(), memorizer.data.end(), [](DataPoint& d){
+        auto& value = d.state.find("H")->second;
+        value *= 1000;
+    });
     Plotter::visit(memorizer.data);
 
     return 0;
